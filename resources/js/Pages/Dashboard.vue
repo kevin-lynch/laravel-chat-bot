744539f08e17d9cdd01303679e362212bebfe3d7
kevin lynch <kevin@kpldesigns.co.uk>
2023-11-15 15:41:50 +0000

    Added link

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -14,7 +14,11 @@ import { Head } from '@inertiajs/vue3';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <a href="/chat" class="underline text-blue-600 hover:text-blue-800 hover:no-underline">
+                            Go To Chat
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
